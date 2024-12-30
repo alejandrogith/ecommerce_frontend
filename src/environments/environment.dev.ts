f83a55c767fb9dfa4dfb228d7eeb2c348a234f6f
alejandro <alegonzalezdeveloper@gmail.com>
2024-12-30 17:39:23 -0600

    first commit

---
+++ src/environments/environment.dev.ts
@@ -0,0 +1,6 @@
+export const environment = {
+  production: false,
+  apiUrl: 'http://localhost:3000',
+
+  state: 'estamos en desarrollo',
+};
---
+++ src/environments/environment.prod.ts
@@ -0,0 +1,4 @@
+export const environment = {
+  production: true,
+  state: 'estamos en producción',
+};
