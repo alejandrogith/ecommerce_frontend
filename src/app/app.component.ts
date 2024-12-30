a0d77c3d3204fb05e442970c0fde7d74c9b431cc
alejandro <alegonzalezdeveloper@gmail.com>
2024-12-30 17:47:33 -0600

    secound commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,6 @@
 import { ChangeDetectionStrategy, Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { environment } from '@src/environments/environment';
 
 @Component({
   selector: 'app-root',
@@ -8,4 +9,8 @@ import { RouterOutlet } from '@angular/router';
   styleUrl: './app.component.scss',
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
-export class AppComponent {}
+export class AppComponent {
+  constructor() {
+    console.log(environment.state);
+  }
+}
